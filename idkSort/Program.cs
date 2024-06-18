44c231cb6811b74726d228b71ef70404cec818af
Kaufko <99084056+Kaufko@users.noreply.github.com>
2024-06-18 15:42:57 +0200

    Updated comments

--- idkSort/Program.cs
+++ idkSort/Program.cs
@@ -1,6 +1,8 @@
 using Plotly.NET;
 
+//123 = seed (so testing is easier and more correct)
 Random random = new Random(123);
+//creates a random list of ints
 List<int> CreateRandomList(int size)
 {
     List<int> list = new List<int>();
@@ -13,18 +15,22 @@ List<int> CreateRandomList(int size)
 
 int IdkSort(List<int> list, int size)
 {
+    //timesRan is for testing
     int timesRan = 0;
     int tempIndex;
     int addIndex;
     if (size < 500) addIndex = 10;
+    //finds suitable number to increase the index by
     else addIndex = (int)Math.Pow(10, size.ToString().Length / 2.55f);
 
+    //iterates over every number in the array and sorts it
     for (int i = 0; i < size; i++)
     {
         tempIndex = addIndex;
         while (true)
         {
             timesRan++;
+            //checks if index isn't out of range and if the current number is higher than the number at the temporary index
             if (tempIndex < size && list[i] > list[tempIndex])
             {
                 tempIndex += addIndex;
@@ -32,10 +38,13 @@ int IdkSort(List<int> list, int size)
             }
             else
             {
+                //removes addIndex from tempIndex so the current index is below correct index
                 tempIndex -= addIndex;
+                //goes up by 1 index and checks if it's the right index to insert at
                 for (int j = tempIndex; j < size; j++)
                 {
                     timesRan++;
+                    //continues if the number it's sorting is higher than current index
                     if (list[i] > list[j])
                     {
                         continue;
@@ -43,7 +52,10 @@ int IdkSort(List<int> list, int size)
                     else
                     {
                         int tempval = list[i];
+                        //checks if the number we are sorting has a lower index than the one we are inserting at the temporary index
+                        //if it is, we have to remove 1 from the index cause the position would not be correct
                         if (j > i) j--;
+                        //removes number it's sorting and inserts it at the correct index
                         list.RemoveAt(i);
                         list.Insert(j, tempval);
                         break;
@@ -56,6 +68,7 @@ int IdkSort(List<int> list, int size)
     return timesRan;
 }
 
+//Testing section
 int Sort(int size)
 {
     int temp = 0;
@@ -68,6 +81,7 @@ int Sort(int size)
 }
 
 
+//this number is used to test arrays with length 1 -> siz
 int siz = 10000;
 
 List<int> resultList = new List<int>();
